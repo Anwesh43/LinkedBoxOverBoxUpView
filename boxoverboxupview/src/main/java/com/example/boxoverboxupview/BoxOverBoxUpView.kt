952fc80dbfe6b0e96f3ea95dd34e6bfbc3329566
Anwesh43 <anweshthecool0@gmail.com>
2021-06-25 01:37:23 +0530

    adding Canvas extension methods

--- boxoverboxupview/src/main/java/com/example/boxoverboxupview/BoxOverBoxUpView.kt
+++ boxoverboxupview/src/main/java/com/example/boxoverboxupview/BoxOverBoxUpView.kt
@@ -27,3 +27,39 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBoxOverBoxUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        val x : Float = size * 0.25f * (1f - 2 * j) * sc2
+        val y : Float = -x
+        save()
+        translate(
+            x,
+            y - (h /2 + y) * sc2
+        )
+        drawRect(
+            RectF(
+                -size / 2,
+                size / 2 -size * sc1,
+                size / 2,
+                size / 2
+            ),
+            paint
+        )
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBOBUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawBoxOverBoxUp(scale, w, h, paint)
+}
