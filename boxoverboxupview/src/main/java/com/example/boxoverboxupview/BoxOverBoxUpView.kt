55d2e0ef4e7f986d2fc09bd1e7aaf893eccf689d
Anwesh43 <anweshthecool0@gmail.com>
2021-06-25 01:52:07 +0530

    adding BOBUNode class

--- boxoverboxupview/src/main/java/com/example/boxoverboxupview/BoxOverBoxUpView.kt
+++ boxoverboxupview/src/main/java/com/example/boxoverboxupview/BoxOverBoxUpView.kt
@@ -126,4 +126,45 @@ class BoxOverBoxUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BOBUNode(var i : Int, val state : State = State()) {
+
+        private var next : BOBUNode? = null
+        private var prev : BOBUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BOBUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBOBUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BOBUNode {
+            var curr : BOBUNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
