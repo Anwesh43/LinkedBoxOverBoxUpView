7d23985cbf56c64806a8e8b15df118d10c611882
Anwesh43 <anweshthecool0@gmail.com>
2021-06-25 02:39:08 +0530

    adding fixes

--- boxoverboxupview/src/main/java/com/example/boxoverboxupview/BoxOverBoxUpView.kt
+++ boxoverboxupview/src/main/java/com/example/boxoverboxupview/BoxOverBoxUpView.kt
@@ -32,16 +32,15 @@ fun Canvas.drawBoxOverBoxUp(scale : Float, w : Float, h : Float, paint : Paint) 
     val size : Float = Math.min(w, h) / sizeFactor
     val sc1 : Float = scale.divideScale(0, parts)
     val sc2 : Float = scale.divideScale(1, parts)
-    val sc3 : Float = scale.divideScale(2, parts)
     save()
     translate(w / 2, h / 2)
     for (j in 0..1) {
-        val x : Float = size * 0.25f * (1f - 2 * j) * sc2
+        val x : Float = size * 0.25f * (1f - 2 * j) * sc2.divideScale(0, parts)
         val y : Float = -x
         save()
         translate(
             x,
-            y - (h /2 + y) * sc2
+            y - (h /2 + size) * scale.divideScale(2 + j, parts)
         )
         drawRect(
             RectF(
