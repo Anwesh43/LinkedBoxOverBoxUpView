53af775cb09070b248008403fbff87cd354bcb68
Anwesh43 <anweshthecool0@gmail.com>
2021-06-25 01:29:22 +0530

    adding constants and libraries

--- boxoverboxupview/src/main/java/com/example/boxoverboxupview/BoxOverBoxUpView.kt
+++ boxoverboxupview/src/main/java/com/example/boxoverboxupview/BoxOverBoxUpView.kt
@@ -1,2 +1,25 @@
 package com.example.boxoverboxupview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.app.Activity
+import android.content. Context
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#004D40",
+    "#FFD600",
+    "#00C853",
+    "#6200EA"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val delay : Long = 20
+val sizeFactor : Float = 3.9f
+val backColor : Int = Color.parseColor("#BDBDBD")
